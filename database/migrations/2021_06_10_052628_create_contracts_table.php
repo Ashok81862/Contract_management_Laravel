6c66cfe6248f4077515d9e375698de74c979c314
Ashok812 <chhetriarash81@gmail.com>
2021-06-10 11:14:08 +0545

    Contract Model and Migration Created

---
+++ database/migrations/2021_06_10_052628_create_contracts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContractsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->date('contract_date')->nullable();
+            $table->string('subject')->nullable();
+            $table->longText('full_text')->nullable();
+            $table->boolean('is_signed')->default(0)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contracts');
+    }
+}
